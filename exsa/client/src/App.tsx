555e299808a55f388ff91d626d8c485953e7fd1a
delamaterj <delamaterj@ufl.edu>
2026-02-11 17:46:09 -0500

    Implemented Button interface

--- exsa/client/src/App.tsx
+++ exsa/client/src/App.tsx
@@ -1,38 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import Button from './components/Button';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <nav className="backdrop1">
       <div>
         <p>Logo</p>
       </div>
-      <div>
-        <p>Home</p>
-      </div>
-      <div>
-        <p>About EXSA</p>
-      </div>
-      <div>
-        <p>Our Members</p>
-      </div>
+      <Button title="Home" disabled={false} url="/Home"/>
+      <Button title="About EXSA" disabled={false} url="/AboutEXSA"/>
+      <Button title="Our Members" disabled={false} url="/OurMembers"/>
       </nav>
       <div className="backdrop2">
         <h1>Welcome to EXSA</h1>
+      </div>
+      <div>
+        <br/>
+        <p>This is the home page. This is where the important content will go.</p>
+        <br/>
       </div>
       <footer className="backdrop1">
       <p>Address</p>
       <p>Email</p>
       <p>© 2025 EXSA. All rights reserved.</p>
-      <div>
-        <p>Facebook Button</p>
-      </div>
-      <div>
-        <p>Instgram Button</p>
-      </div>
+      <Button title="Facebook" disabled={false} url="/Facebook"/>
+      <Button title="Instagram" disabled={false} url="/Instagram"/>
       </footer>
     </>
     /*
@@ -62,4 +55,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
---
+++ exsa/client/src/components/Button.tsx
@@ -0,0 +1,45 @@
+import { useNavigate } from 'react-router-dom';
+
+interface ButtonProp {
+
+  title: string;
+  disabled: boolean;
+  url: string;
+
+}
+
+function Button({title, disabled, url}: ButtonProp){
+
+    //const navigate = useNavigate();
+    function handleClickBack() {
+
+        //navigate(-1);
+
+    }
+
+    function handleClickNav() {
+        //navigate(url);
+    }
+
+    function handleClick() {
+
+    }
+
+    if (title === "Go Back") {
+        return (
+            <button disabled={disabled} className="btn-primary" onClick={handleClickBack}>{title}</button>
+        );
+    }
+    else if (url !== "") {
+        return (
+            <button disabled={disabled} className="btn-primary" onClick={handleClickNav}>{title}</button>
+        );
+    }
+    else {
+        return (
+            <button disabled={disabled} onClick={handleClick}>{title}</button>
+        );
+    }
+}
+
+export default Button;
