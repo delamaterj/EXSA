771b6cd4aa62eb0c1051a8586abcdfa15f7b10a8
delamaterj <delamaterj@ufl.edu>
2026-07-09 15:03:33 -0400

    reconfiguring cors variable

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+        origin: process.env.CORS_ORIGIN,
     })
 );
 app.use(express.json());
