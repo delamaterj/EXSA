61e681ef954efba7783b0e316f0d2bcb518c745d
delamaterj <delamaterj@ufl.edu>
2026-08-10 17:33:44 -0400

    updated job with actual performance

--- server/src/jobs/cleanupVerificationTokens.ts
+++ server/src/jobs/cleanupVerificationTokens.ts
@@ -1,11 +1,29 @@
 import cron from "node-cron";
+import pool from "../config/db";
 
 export function initializeVerificationTokenCleanup() {
 
-    cron.schedule("* * * * *", () => {
-        console.log(
-            "[CRON] Verification token cleanup job executed"
-        );
-    });
+    cron.schedule("0 0 * * *", async () => {
 
+        const client = await pool.connect();
+
+        try {
+            const result = await client.query(
+                `DELETE FROM email_verification_tokens
+                WHERE expires_at < NOW()`);
+
+            /*console.log(
+                `[CRON] Deleted ${result.rowCount ?? 0} expired verification tokens`
+            );*/
+        }
+        catch (err) {
+            console.error(
+                "[CRON] Failed to delete expired verification tokens:",
+                err
+            );
+        }
+        finally {
+            client.release();
+        }
+    });
 }
